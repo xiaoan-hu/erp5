280e6772b83967b53e40c9c1d4712206ca226577
xiaoan-hu <7237118@qq.com>
2021-03-24 20:23:34 +0800

    添加三个定时任务类，分别同步商品列表，获取5分钟内付款的订单，根据库存更新创建发货单

---
+++ src/main/java/ltd/yuhan/erp/cron/ShoppingOrderGet.java
@@ -0,0 +1,82 @@
+package ltd.yuhan.erp.cron;
+
+import com.alibaba.ocean.rawsdk.ApiExecutor;
+import com.alibaba.ocean.rawsdk.common.SDKResult;
+import com.alibaba.trade.param.AlibabaOpenplatformTradeModelProductItemInfo;
+import com.alibaba.trade.param.AlibabaOpenplatformTradeModelTradeInfo;
+import com.alibaba.trade.param.AlibabaTradeGetSellerOrderListParam;
+import com.alibaba.trade.param.AlibabaTradeGetSellerOrderListResult;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ltd.yuhan.erp.mapper.ShoppingOrderDetailMapper;
+import ltd.yuhan.erp.mapper.ShoppingOrderMapper;
+import ltd.yuhan.erp.model.ShoppingOrder;
+import ltd.yuhan.erp.model.ShoppingOrderDetail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+@Service
+@EnableScheduling
+public class ShoppingOrderGet {
+    @Autowired
+    private ShoppingOrderMapper shoppingOrderMapper;
+    @Autowired
+    private ShoppingOrderDetailMapper shoppingOrderDetailMapper;
+    private static final Logger logger = LoggerFactory.getLogger(ShoppingOrderGet.class);
+
+    //指定时间间隔5分钟
+    @Scheduled(fixedRate=1000*60*5)
+    public void getOrder() throws JsonProcessingException {
+
+
+
+        //设置appkey和密钥(seckey)
+        ApiExecutor apiExecutor = new ApiExecutor("4855454","VI90KjSOEZ");
+
+        AlibabaTradeGetSellerOrderListParam orderListParam= new AlibabaTradeGetSellerOrderListParam();
+        // TODO 添加参数每五分钟抓取一次等待卖家发货的订单列表
+        orderListParam.setOrderStatus("waitsellersend");
+        //现在时间往前推五分钟，1分钟等于6万毫秒
+        orderListParam.setCreateStartTime(new Date(System.currentTimeMillis() -60000*5));
+        SDKResult<AlibabaTradeGetSellerOrderListResult> execute = apiExecutor.execute(orderListParam,"894eab17-a695-4607-a549-10118a3152ec");
+        ObjectMapper objectMapper = new ObjectMapper();
+        AlibabaTradeGetSellerOrderListResult result = execute.getResult();
+        AlibabaOpenplatformTradeModelTradeInfo[] result1 = result.getResult();
+        logger.info("导入阿里销售量数量"+result1.length);
+        for (AlibabaOpenplatformTradeModelTradeInfo info: result1
+             ) {
+
+            //如果查不到订单记录再插入，已支付订单暂不考虑更新的情况
+            if (shoppingOrderMapper.selectByPrimaryKey(info.getBaseInfo().getId()) == null){
+                ShoppingOrder shoppingOrder = new ShoppingOrder();
+                shoppingOrder.setCreatetime(info.getBaseInfo().getCreateTime());
+                shoppingOrder.setShopper(info.getBaseInfo().getBuyerLoginId());
+                shoppingOrder.setId(info.getBaseInfo().getId());
+                shoppingOrder.setTotalprice(info.getBaseInfo().getTotalAmount());
+                shoppingOrder.setStatus(0);
+                shoppingOrderMapper.insert(shoppingOrder);
+
+                AlibabaOpenplatformTradeModelProductItemInfo[] productItems = info.getProductItems();
+                for (AlibabaOpenplatformTradeModelProductItemInfo p: productItems
+                ) {
+                    ShoppingOrderDetail shoppingOrderDetail = new ShoppingOrderDetail();
+                    shoppingOrderDetail.setShoppingorderid(info.getBaseInfo().getId());
+                    shoppingOrderDetail.setTotalprice(p.getItemAmount());
+                    shoppingOrderDetail.setQty(p.getQuantity().intValue());
+                    shoppingOrderDetail.setGoodsid(p.getSkuID());
+                    shoppingOrderDetailMapper.insert(shoppingOrderDetail);
+                }
+
+            }
+
+
+        }
+
+
+    }
+}
---
+++ src/main/java/ltd/yuhan/erp/cron/WarehouseOutGet.java
@@ -0,0 +1,105 @@
+package ltd.yuhan.erp.cron;
+
+import ltd.yuhan.erp.mapper.ShoppingOrderDetailMapper;
+import ltd.yuhan.erp.mapper.ShoppingOrderMapper;
+import ltd.yuhan.erp.mapper.WarehouseOutMapper;
+import ltd.yuhan.erp.mapper.WarehouseoutinfoDao;
+import ltd.yuhan.erp.model.ShoppingOrder;
+import ltd.yuhan.erp.model.WarehouseOut;
+import ltd.yuhan.erp.model.Warehouseoutinfo;
+import ltd.yuhan.erp.service.GoodsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+
+@Service
+@EnableScheduling
+//根据未完成订单和库存生成发货单，准备定时运行
+public class WarehouseOutGet {
+
+    @Autowired
+    private ShoppingOrderMapper shoppingOrderMapper;
+    @Autowired
+    private ShoppingOrderDetailMapper shoppingOrderDetailMapper;
+    @Autowired
+    private GoodsService goodsService;
+    @Autowired
+    private WarehouseOutMapper warehouseOutMapper;
+    @Autowired
+    private WarehouseoutinfoDao warehouseoutinfoDao;
+    private static final Logger logger = LoggerFactory.getLogger(WarehouseOutGet.class);
+
+
+
+    //如果order处于未完结状态
+    //找到所有orderdetail记录，
+    //指定时间间隔5分钟
+    @Scheduled(fixedRate=1000*60*5)
+    public void getWarehouseOut(){
+
+        //获取未完成order列表，0代表未完成，1代表完成
+        List<ShoppingOrder> shoppingOrderByTerm = shoppingOrderMapper.getShoppingOrderByTerm("0", "", "", "", "");
+        for (ShoppingOrder s:shoppingOrderByTerm
+             ) {
+            //获取shopping id
+            Long soId = s.getId();
+            Warehouseoutinfo warehouseoutinfo = new Warehouseoutinfo();
+            //拿到最大的infoid的现有的最大值，新建的值在最大的基础上加1
+            int infoId = warehouseoutinfoDao.selectMaxPrimaryKey() + 1;
+            warehouseoutinfo.setOutinfoid(infoId);
+            warehouseoutinfo.setOrderid(soId);
+            warehouseoutinfo.setCreatetime(new Date());
+            warehouseoutinfo.setStatus(0);
+
+            List<Map> maps = shoppingOrderDetailMapper.selectSoDetailBySoId(soId);
+            //循环订单中的每种商品，根据未发货量和库存量，计算出发货单应发货量
+            for (int i = 0; i <maps.size() ; i++) {
+                Map map = maps.get(i);
+                //获取goodsid
+                Long goodsId = (Long)map.get("goodsId");
+                //根据goodsId取得现有库存
+                int goodsQty = goodsService.getGoodsQty(goodsId);
+                //取得该订单销售量
+                int saleQty = (int)map.get("qty");
+
+                //计算该订单该商品已发货量
+                int goodsOutTotal = 0;
+                List<WarehouseOut> warehouseOutByTerm = warehouseOutMapper.getWarehouseOutByTerm(goodsId, soId);
+                for (WarehouseOut out: warehouseOutByTerm
+                     ) {
+                    goodsOutTotal += out.getQty();
+                }
+                //库存量和该订单的未发量的两者较小的，为应发货量
+                int outGoods = Math.min(goodsQty, (saleQty - goodsOutTotal));
+                if(outGoods > 0){
+                    //一个订单进而有多个商品，可能之前已经创建过了这条info里，只有没有创建过，才再创建一次
+                    if(warehouseoutinfoDao.selectMaxPrimaryKey() < infoId){
+                        warehouseoutinfoDao.insert(warehouseoutinfo);
+                    }
+
+                    WarehouseOut warehouseOut = new WarehouseOut();
+                    warehouseOut.setGoodsid(goodsId);
+                    TimeZone.setDefault(TimeZone.getTimeZone("GMT+8:00"));
+                    warehouseOut.setCreatetime(new Date());
+                    warehouseOut.setOrderid(soId);
+                    warehouseOut.setQty(outGoods);
+                    //出库单状态设为0，代表生成出库单，但未发货状态
+                    warehouseOut.setStatus(0);
+                    warehouseOut.setInfoid(infoId);
+                    warehouseOutMapper.insert(warehouseOut);
+                }
+            }
+
+        }
+        logger.info("根据库存变更生成发货单成功");
+    }
+
+}
